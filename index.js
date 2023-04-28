0d59a890311eab8ab47bfea3849129fec1eed15e
Brent <124935518+Brent-Bew-Canon@users.noreply.github.com>
2023-04-28 13:31:58 -0700

    cleaned up extra comments

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 
-// THEN I am shown a 300x200 pixel image that matches the criteria I entered
-
 const shapes = require("./lib/shapes")
 const fs = require('fs');
 const inquirer = require('inquirer');
